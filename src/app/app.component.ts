32c9bd9f2c807d998c59e18c2bdb80f52d271c30
Suzowy <suzowy@gmail.com>
2024-10-10 13:10:32 +0200

    google

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, OnDestroy, HostListener, Inject, PLATFORM_ID } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 import { ScrollService } from './services/scroll.service';
 import { isPlatformBrowser } from '@angular/common';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -10,13 +12,23 @@ import { isPlatformBrowser } from '@angular/common';
 export class AppComponent implements OnInit, OnDestroy {
   private cleanupScrollEffect!: () => void;
   showScrollToTopButton = false;
+isLoading: any;
 
-  constructor(private scrollService: ScrollService, @Inject(PLATFORM_ID) private platformId: Object) {}
+  constructor(private scrollService: ScrollService,
+              @Inject(PLATFORM_ID) private platformId: Object,
+              private router: Router) {}
 
   ngOnInit() {
     if (isPlatformBrowser(this.platformId)) {
       this.cleanupScrollEffect = this.scrollService.initializeScrollEffect();
     }
+
+
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      this.scrollToTop();
+    });
   }
 
   ngOnDestroy() {
@@ -31,12 +43,15 @@ export class AppComponent implements OnInit, OnDestroy {
     this.showScrollToTopButton = window.scrollY > scrollThreshold;
   }
 
-  // Función para desplazar suavemente la página hasta el elemento con ID 'cabecera'
+
   scrollToTop(): void {
     if (isPlatformBrowser(this.platformId)) {
       const cabeceraElement = document.getElementById('cabecera');
       if (cabeceraElement) {
         cabeceraElement.scrollIntoView({ behavior: 'smooth' });
+      } else {
+
+        window.scrollTo({ top: 0, behavior: 'smooth' });
       }
     }
   }
